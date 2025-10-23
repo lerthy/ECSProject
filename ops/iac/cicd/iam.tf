c9a3bfeca6fac0432249df12468a96d946c93bb4
Your Name <lerthy@example.com>
2025-10-23 13:43:53 +0200

    Add comprehensive IAM permissions for infrastructure creation
    - Add IAM permissions for role creation and management
    - Add S3 permissions for bucket creation and configuration
    - Add EC2 permissions for VPC, subnet, security group creation
    - Add ECS permissions for cluster and service management
    - Add RDS permissions for database creation and management
    - Add Secrets Manager permissions for secret creation
    - Add CloudWatch permissions for dashboards and alarms
    - Add CloudWatch Logs permissions for log group creation
    - Add SNS permissions for topic creation and management
    - Add Athena permissions for workgroup and database creation
    - Add WAFv2 permissions for web ACL creation
    - Add CloudFront permissions for distribution creation
    - Add DMS permissions for replication instance creation
    - Add ECR permissions for repository creation
    - Add X-Ray permissions for group creation
    - Resolves all AccessDenied errors in terraform apply phase

--- ops/iac/cicd/iam.tf
+++ ops/iac/cicd/iam.tf
@@ -352,6 +352,406 @@ resource "aws_iam_role_policy" "codebuild" {
           "kms:ListAliases"
         ]
         Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:PassRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:GetBucketLocation",
+          "s3:GetBucketVersioning",
+          "s3:PutBucketVersioning",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:DeleteBucketPolicy",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketEncryption",
+          "s3:PutBucketEncryption",
+          "s3:GetBucketAccelerateConfiguration",
+          "s3:PutBucketAccelerateConfiguration",
+          "s3:GetBucketIntelligentTieringConfiguration",
+          "s3:PutBucketIntelligentTieringConfiguration",
+          "s3:GetBucketLogging",
+          "s3:PutBucketLogging",
+          "s3:GetBucketNotification",
+          "s3:PutBucketNotification",
+          "s3:GetBucketReplication",
+          "s3:PutBucketReplication",
+          "s3:GetBucketTagging",
+          "s3:PutBucketTagging",
+          "s3:GetBucketWebsite",
+          "s3:PutBucketWebsite",
+          "s3:DeleteBucketWebsite",
+          "s3:GetBucketCors",
+          "s3:PutBucketCors",
+          "s3:DeleteBucketCors",
+          "s3:GetBucketLifecycleConfiguration",
+          "s3:PutBucketLifecycleConfiguration",
+          "s3:DeleteBucketLifecycleConfiguration"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:DescribeVpcs",
+          "ec2:ModifyVpcAttribute",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:DescribeSubnets",
+          "ec2:ModifySubnetAttribute",
+          "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
+          "ec2:DescribeInternetGateways",
+          "ec2:AttachInternetGateway",
+          "ec2:DetachInternetGateway",
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:DescribeRouteTables",
+          "ec2:CreateRoute",
+          "ec2:DeleteRoute",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:CreateNatGateway",
+          "ec2:DeleteNatGateway",
+          "ec2:DescribeNatGateways",
+          "ec2:AllocateAddress",
+          "ec2:ReleaseAddress",
+          "ec2:DescribeAddresses",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:DescribeSecurityGroups",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupEgress",
+          "ec2:CreateNetworkAcl",
+          "ec2:DeleteNetworkAcl",
+          "ec2:DescribeNetworkAcls",
+          "ec2:CreateNetworkAclEntry",
+          "ec2:DeleteNetworkAclEntry",
+          "ec2:AssociateNetworkAcl",
+          "ec2:DisassociateNetworkAcl",
+          "ec2:CreateVpcEndpoint",
+          "ec2:DeleteVpcEndpoint",
+          "ec2:DescribeVpcEndpoints",
+          "ec2:ModifyVpcEndpoint",
+          "ec2:CreateVpcPeeringConnection",
+          "ec2:DeleteVpcPeeringConnection",
+          "ec2:DescribeVpcPeeringConnections",
+          "ec2:AcceptVpcPeeringConnection",
+          "ec2:RejectVpcPeeringConnection",
+          "ec2:CreateVpcPeeringConnection",
+          "ec2:DeleteVpcPeeringConnection",
+          "ec2:DescribeVpcPeeringConnections",
+          "ec2:AcceptVpcPeeringConnection",
+          "ec2:RejectVpcPeeringConnection"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:CreateCluster",
+          "ecs:DeleteCluster",
+          "ecs:DescribeClusters",
+          "ecs:UpdateCluster",
+          "ecs:CreateService",
+          "ecs:DeleteService",
+          "ecs:DescribeServices",
+          "ecs:UpdateService",
+          "ecs:CreateTaskDefinition",
+          "ecs:DeleteTaskDefinition",
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition",
+          "ecs:DeregisterTaskDefinition",
+          "ecs:ListTaskDefinitions",
+          "ecs:CreateCapacityProvider",
+          "ecs:DeleteCapacityProvider",
+          "ecs:DescribeCapacityProviders",
+          "ecs:UpdateCapacityProvider",
+          "ecs:CreateClusterCapacityProvider",
+          "ecs:DeleteClusterCapacityProvider",
+          "ecs:DescribeClusterCapacityProviders",
+          "ecs:UpdateClusterCapacityProvider"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "rds:CreateDBInstance",
+          "rds:DeleteDBInstance",
+          "rds:DescribeDBInstances",
+          "rds:ModifyDBInstance",
+          "rds:CreateDBCluster",
+          "rds:DeleteDBCluster",
+          "rds:DescribeDBClusters",
+          "rds:ModifyDBCluster",
+          "rds:CreateDBSubnetGroup",
+          "rds:DeleteDBSubnetGroup",
+          "rds:DescribeDBSubnetGroups",
+          "rds:ModifyDBSubnetGroup",
+          "rds:CreateDBParameterGroup",
+          "rds:DeleteDBParameterGroup",
+          "rds:DescribeDBParameterGroups",
+          "rds:ModifyDBParameterGroup",
+          "rds:CreateDBClusterParameterGroup",
+          "rds:DeleteDBClusterParameterGroup",
+          "rds:DescribeDBClusterParameterGroups",
+          "rds:ModifyDBClusterParameterGroup",
+          "rds:CreateDBSnapshot",
+          "rds:DeleteDBSnapshot",
+          "rds:DescribeDBSnapshots",
+          "rds:CreateDBClusterSnapshot",
+          "rds:DeleteDBClusterSnapshot",
+          "rds:DescribeDBClusterSnapshots",
+          "rds:RestoreDBInstanceFromDBSnapshot",
+          "rds:RestoreDBClusterFromSnapshot",
+          "rds:CreateDBInstanceReadReplica",
+          "rds:CreateDBClusterReadReplica",
+          "rds:PromoteReadReplica",
+          "rds:PromoteReadReplicaDBCluster",
+          "rds:StartDBInstance",
+          "rds:StopDBInstance",
+          "rds:RebootDBInstance",
+          "rds:AddTagsToResource",
+          "rds:RemoveTagsFromResource",
+          "rds:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:CreateSecret",
+          "secretsmanager:DeleteSecret",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue",
+          "secretsmanager:UpdateSecret",
+          "secretsmanager:TagResource",
+          "secretsmanager:UntagResource",
+          "secretsmanager:ListSecrets"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutDashboard",
+          "cloudwatch:DeleteDashboards",
+          "cloudwatch:GetDashboard",
+          "cloudwatch:ListDashboards",
+          "cloudwatch:PutMetricData",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:PutMetricAlarm",
+          "cloudwatch:DeleteAlarms",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:SetAlarmState",
+          "cloudwatch:EnableAlarmActions",
+          "cloudwatch:DisableAlarmActions"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:DeleteLogGroup",
+          "logs:DescribeLogGroups",
+          "logs:PutRetentionPolicy",
+          "logs:DeleteRetentionPolicy",
+          "logs:CreateLogStream",
+          "logs:DeleteLogStream",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents",
+          "logs:CreateLogStream",
+          "logs:DeleteLogStream",
+          "logs:DescribeLogStreams",
+          "logs:PutLogEvents",
+          "logs:GetLogEvents",
+          "logs:FilterLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "sns:CreateTopic",
+          "sns:DeleteTopic",
+          "sns:GetTopicAttributes",
+          "sns:SetTopicAttributes",
+          "sns:Subscribe",
+          "sns:Unsubscribe",
+          "sns:ListSubscriptions",
+          "sns:ListSubscriptionsByTopic",
+          "sns:Publish",
+          "sns:AddPermission",
+          "sns:RemovePermission",
+          "sns:TagResource",
+          "sns:UntagResource",
+          "sns:ListTagsForResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "athena:CreateWorkGroup",
+          "athena:DeleteWorkGroup",
+          "athena:GetWorkGroup",
+          "athena:ListWorkGroups",
+          "athena:UpdateWorkGroup",
+          "athena:CreateDataCatalog",
+          "athena:DeleteDataCatalog",
+          "athena:GetDataCatalog",
+          "athena:ListDataCatalogs",
+          "athena:UpdateDataCatalog",
+          "athena:CreateDatabase",
+          "athena:DeleteDatabase",
+          "athena:GetDatabase",
+          "athena:ListDatabases",
+          "athena:UpdateDatabase",
+          "athena:CreateTable",
+          "athena:DeleteTable",
+          "athena:GetTable",
+          "athena:ListTables",
+          "athena:UpdateTable"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "wafv2:CreateWebACL",
+          "wafv2:DeleteWebACL",
+          "wafv2:GetWebACL",
+          "wafv2:ListWebACLs",
+          "wafv2:UpdateWebACL",
+          "wafv2:AssociateWebACL",
+          "wafv2:DisassociateWebACL",
+          "wafv2:GetWebACLForResource",
+          "wafv2:ListResourcesForWebACL"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudfront:CreateDistribution",
+          "cloudfront:DeleteDistribution",
+          "cloudfront:GetDistribution",
+          "cloudfront:ListDistributions",
+          "cloudfront:UpdateDistribution",
+          "cloudfront:CreateCloudFrontOriginAccessIdentity",
+          "cloudfront:DeleteCloudFrontOriginAccessIdentity",
+          "cloudfront:GetCloudFrontOriginAccessIdentity",
+          "cloudfront:ListCloudFrontOriginAccessIdentities",
+          "cloudfront:UpdateCloudFrontOriginAccessIdentity",
+          "cloudfront:CreateInvalidation",
+          "cloudfront:GetInvalidation",
+          "cloudfront:ListInvalidations",
+          "cloudfront:CreateStreamingDistribution",
+          "cloudfront:DeleteStreamingDistribution",
+          "cloudfront:GetStreamingDistribution",
+          "cloudfront:ListStreamingDistributions",
+          "cloudfront:UpdateStreamingDistribution"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dms:CreateReplicationInstance",
+          "dms:DeleteReplicationInstance",
+          "dms:DescribeReplicationInstances",
+          "dms:ModifyReplicationInstance",
+          "dms:CreateEndpoint",
+          "dms:DeleteEndpoint",
+          "dms:DescribeEndpoints",
+          "dms:ModifyEndpoint",
+          "dms:CreateReplicationTask",
+          "dms:DeleteReplicationTask",
+          "dms:DescribeReplicationTasks",
+          "dms:ModifyReplicationTask",
+          "dms:StartReplicationTask",
+          "dms:StopReplicationTask",
+          "dms:TestConnection"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:CreateRepository",
+          "ecr:DeleteRepository",
+          "ecr:DescribeRepositories",
+          "ecr:UpdateRepository",
+          "ecr:PutImage",
+          "ecr:GetImage",
+          "ecr:ListImages",
+          "ecr:DeleteImage",
+          "ecr:BatchDeleteImage",
+          "ecr:PutLifecyclePolicy",
+          "ecr:GetLifecyclePolicy",
+          "ecr:DeleteLifecyclePolicy",
+          "ecr:GetLifecyclePolicyPreview",
+          "ecr:StartLifecyclePolicyPreview",
+          "ecr:GetRepositoryPolicy",
+          "ecr:SetRepositoryPolicy",
+          "ecr:DeleteRepositoryPolicy"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "xray:CreateGroup",
+          "xray:DeleteGroup",
+          "xray:GetGroup",
+          "xray:ListGroups",
+          "xray:UpdateGroup",
+          "xray:CreateSamplingRule",
+          "xray:DeleteSamplingRule",
+          "xray:GetSamplingRules",
+          "xray:GetSamplingTargets",
+          "xray:PutTelemetryRecords",
+          "xray:PutTraceSegments",
+          "xray:GetTraceGraph",
+          "xray:GetTraceSummaries",
+          "xray:BatchGetTraces",
+          "xray:GetTrace",
+          "xray:GetServiceGraph",
+          "xray:GetInsight",
+          "xray:GetInsightSummaries",
+          "xray:GetInsightEvents",
+          "xray:GetInsightImpactGraph"
+        ]
+        Resource = "*"
       }
     ]
   })
